256945c67bba8ca2497d435be215996c7468b945
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-07-09 11:50:28 +0300

    Approach more tddy

--- app/src/test/java/com/daniil/shevtsov/timetravel/PrototypeTest.kt
+++ app/src/test/java/com/daniil/shevtsov/timetravel/PrototypeTest.kt
@@ -18,15 +18,26 @@ class PrototypeTest {
     }
 
     @Test
-    fun `should time 3000`() {
+    fun `should time 1500`() {
         test(
-            time = 3000f,
+            time = 1500f,
             duration = 3000f,
             nodes = listOf(0, 1, 2),
-            expectedMomentIndex = 1,
+            expectedMomentIndex = 0,
             expectedSegmentFraction = 1f
         )
     }
+
+//    @Test
+//    fun `should time 3000`() {
+//        test(
+//            time = 3000f,
+//            duration = 3000f,
+//            nodes = listOf(0, 1, 2),
+//            expectedMomentIndex = 1,
+//            expectedSegmentFraction = 1f
+//        )
+//    }
 
     private fun test(
         time: Float,
@@ -46,6 +57,7 @@ class PrototypeTest {
             calculateSegmentFraction(
                 momentIndex = momentIndex,
                 time = time,
+                duration = duration,
                 nodes = nodes,
             )
         ).isEqualTo(expectedSegmentFraction)
@@ -59,8 +71,22 @@ class PrototypeTest {
         return ((nodes.lastIndex - 1) * (time / duration)).toInt()
     }
 
-    private fun calculateSegmentFraction(momentIndex: Int, time: Float, nodes: List<Int>): Float {
-        return (momentIndex.toFloat()) / (nodes.lastIndex - 1)
+    private fun calculateSegmentFraction(
+        momentIndex: Int,
+        time: Float,
+        duration: Float,
+        nodes: List<Int>
+    ): Float {
+        val segments = nodes.size - 1
+        val timeProgress = time / duration
+        // 0    -> 0
+        // 0.5f -> 1f
+        // 0.51 -> 0f
+        // 0.75 -> 0.5f
+        // 1f   -> 1f
+
+
+        return timeProgress * segments
     }
 
 }
