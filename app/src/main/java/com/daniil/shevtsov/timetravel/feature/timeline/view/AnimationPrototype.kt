e0f2453d9a5022790d01912b1d16238c5763fee6
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2022-07-09 10:25:47 +0300

    Animation and preview work

--- app/src/main/java/com/daniil/shevtsov/timetravel/feature/timeline/view/AnimationPrototype.kt
+++ app/src/main/java/com/daniil/shevtsov/timetravel/feature/timeline/view/AnimationPrototype.kt
@@ -1,20 +1,29 @@
 package com.daniil.shevtsov.timetravel.feature.timeline.view
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.daniil.shevtsov.timetravel.core.ui.theme.AppTheme
+
+enum class AnimationState {
+    Collapsed,
+    Expanded
+}
 
 @Preview
 @ExperimentalAnimationApi
@@ -32,66 +41,63 @@ fun AnimationPrototype(
     val selectedPointColor = AppTheme.colors.backgroundLight
     val backgroundColor = AppTheme.colors.backgroundDarkest
 
-    val scale = scaleShapeTransition(0.1f, 1f, 2000)
-    val animatedLineColor = colorShapeTransition(lineColor, pointColor, 2000)
-
-    Canvas(modifier = modifier
-        .background(backgroundColor)
-        .size(300.dp)
-        .graphicsLayer {
-            scaleX = scale
-            scaleY = scale
-        }) {
-        val canvasSize = size
-        val canvasWidth = size.width
-        val canvasHeight = size.height
-
-        drawRoundRect(
-            size = canvasSize / 2F,
-            color = animatedLineColor,
-            topLeft = Offset(
-                x = canvasWidth / 4F,
-                y = canvasHeight / 3F
-            )
-        )
-
+// Need to remember in order to prevent setting
+// the same state value to the transition during
+// recomposition.
+    val animationTargetState = remember {
+        mutableStateOf(AnimationState.Collapsed)
     }
-}
 
-@Composable
-fun colorShapeTransition(
-    initialValue: Color,
-    targetValue: Color,
-    durationMillis: Int
-): Color {
-    val infiniteTransition = rememberInfiniteTransition()
-    val color by infiniteTransition.animateColor(
-        initialValue = initialValue,
-        targetValue = targetValue,
-        animationSpec = infiniteRepeatable(
-            animation = tween(durationMillis, easing = FastOutSlowInEasing),
-            repeatMode = RepeatMode.Restart
-        )
+// Any state change will trigger animations which
+// are created with this transition to the new state
+    val transition = updateTransition(
+        targetState = animationTargetState.value
     )
 
-    return color
-}
+    val circleAlpha = transition.animateFloat(
+        transitionSpec = { tween(durationMillis = 3000) }
+    ) {
+        if (it == AnimationState.Collapsed) 0f else 1f
+    }
 
-@Composable
-fun scaleShapeTransition(
-    initialValue: Float,
-    targetValue: Float,
-    durationMillis: Int
-): Float {
-    val infiniteTransition = rememberInfiniteTransition()
-    val scale: Float by infiniteTransition.animateFloat(
-        initialValue = initialValue,
-        targetValue = targetValue,
-        animationSpec = infiniteRepeatable(
-            animation = tween(durationMillis),
-            repeatMode = RepeatMode.Restart
-        )
-    )
+    val circleRadius = transition.animateFloat(
+        transitionSpec = { tween(durationMillis = 3000) }
+    ) {
+        if (it == AnimationState.Collapsed) 0f else 1f
+    }
+    Column {
+        Button(onClick = {
+            animationTargetState.value = when (animationTargetState.value) {
+                AnimationState.Collapsed -> AnimationState.Expanded
+                AnimationState.Expanded -> AnimationState.Collapsed
+            }
+        }, colors = ButtonColors()) {
+            Text(
+                "Launch animation",
+                color = AppTheme.colors.textLight,
+                modifier = Modifier
+            )
+        }
 
-    return scale
+        Canvas(
+            modifier = modifier
+                .background(backgroundColor)
+                .size(300.dp)
+        ) {
+            val canvasSize = size
+            val canvasWidth = size.width
+            val canvasHeight = size.height
+
+            drawRoundRect(
+                size = (canvasSize / 2f) * circleRadius.value,
+                color = lineColor.copy(alpha = circleAlpha.value),
+                topLeft = Offset(
+                    x = canvasWidth / 4F,
+                    y = canvasHeight / 3F
+                )
+            )
+
+        }
+    }
+
 }
